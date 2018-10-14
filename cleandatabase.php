6c289d6be20e7cb673c812d67181f9962d2febfb
Bob Carlson <b0bcarlson@users.noreply.github.com>
2018-10-13 19:25:15 -0500

    Update cleandatabase.php

--- cleandatabase.php
+++ cleandatabase.php
@@ -1,6 +1,8 @@
 <?php
 include("config.php");
-for ($n = 1; n <= 3; n++){
+for ($n = 1; $n <= 3; $n++){
         $table = "words".$n;
-        $conn->query("DELETE FROM $table WHERE ((created < (NOW() - INTERVAL $days DAY)))");
+        $inv = $days * $n;
+        $sens = intdiv($count, $n) + 1;
+        $conn->query("DELETE FROM $table WHERE ((created < (NOW() - INTERVAL $days DAY) AND count < $sens))");
 }
