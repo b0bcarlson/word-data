42d910f91a5820850f5c49628977dc313710bf16
Bob Carlson <b0bcarlson@users.noreply.github.com>
2018-12-17 11:45:45 -0600

    Update wordscript.php
    This marks a substantial change to the overall program. Everything has been significantly simplified, and runs much better now.

--- wordscript.php
+++ wordscript.php
@@ -1,22 +1,8 @@
 <?php
 include("config.php");
-$text = explode(" ", preg_replace("/[^A-Z0-9 ]/", '', strtoupper($_GET['text'])));
-
-for ($n = 3; $n > 0; $n--) {
-        if (count($text) >= $n) {
-                for ($i = 0; $i <= count($text) - $n; $i++) {
-                        $word = trim($text[$i]);
-                        for ($index = 1; $index < $n; $index++){
-                                $word = $word." ".trim($text[$i+$index]);
-                        }
-                        if ($word != "" && $word != " " && strlen($word) < ($n * 30) && strpos($word, "HTTP") === false && strpos($word, "HTTPS") === false) {
-                                if (count(explode(" ", trim($word))) == $n) {
-                                        $word = trim($word);
-                                        $name = "words".$n;
-                                        $query = "INSERT INTO $name (word, count) VALUES ('$word','1') ON DUPLICATE KEY UPDATE count = count + 1, created = NOW()";
-                                        $result = $conn->query($query);
-                                }
-                        }
-                }
-        }
+$arr = json_decode(getopt('f:')['f'], true);
+foreach ($arr as $word => $count) {
+        $name = "words" . count(explode(" ", $word));
+        $query = "INSERT INTO $name (word, count) VALUES ('$word', '$count') ON DUPLICATE KEY UPDATE count = count + $count, created = NOW()";
+        $result = $conn->query($query);
 }
