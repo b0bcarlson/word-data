e26ce0b77c0ea03034c6d6cc6a5b91f27e3b422c
Bob Carlson <b0bcarlson@users.noreply.github.com>
2019-01-02 17:17:02 -0600

    Update getwords.py

--- getwords.py
+++ getwords.py
@@ -11,10 +11,12 @@ r = praw.Reddit(user_agent='Most common words 1.0', site_name='ibs')
 already_done = set()
 all_comments = r.subreddit('all').comments(limit=20)
 users = ["automoderator", "bot", "michaelm40", "kakrail69"]
-disallowed = ["i'm a bot", "i am a bot", "your post has been removed", "live streaming free", "livestreaming free", "live stream free", "lives$words = defaultdict(int)
+disallowed = ["i'm a bot", "i am a bot", "your post has been removed", "live streaming free", "livestreaming free", "live stream free", "livestream free", "free livestream", "free live stream", "http", "à", "å", "ä", "ç", "č", "é", "è", "í", "ñ", "ò", "ø", "ö", "ù", "ü", "ß"]
+words = defaultdict(int)
 for comment in all_comments:
     if comment.id not in already_done:
-        if not any(n in comment.author.name.lower() for n in users) and not any(i in comment.body.lower().encode('utf-8') for i in disallowed)$                already_done.add(comment.id)
+        if not any(n in comment.author.name.lower() for n in users) and not any(i in comment.body.lower().encode('utf-8') for i in disallowed) and comment.body.count(" ") > 1:
+                already_done.add(comment.id)
                 co= re.escape(comment.body.encode('utf-8'))
                 co = re.sub(r'[^A-Z \n]', r'', co.upper())
                 if not re.search(r"(\s+|^\s*)(\s*[A-Z]+\s*)\s+\2\s+\2",co, re.MULTILINE):
