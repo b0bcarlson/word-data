12e780eef7a16ad76fe215592cb724f70461110a
Bob Carlson <b0bcarlson@users.noreply.github.com>
2019-01-03 09:41:32 -0600

    Update wordscript.php
    Made the script 100s of times faster by inserting many rows at once.

--- wordscript.php
+++ wordscript.php
@@ -1,8 +1,12 @@
 <?php
 include("config.php");
 $arr = json_decode(getopt('f:')['f'], true);
-foreach ($arr as $word => $count) {
-        $name = "words" . count(explode(" ", $word));
-        $query = "INSERT INTO $name (word, count) VALUES ('$word', '$count') ON DUPLICATE KEY UPDATE count = count + $count, created = NOW()";
+$sql = array();
+foreach( $arr as $word => $count ) {
+        $name = "words" . count(explode(" ",$word));
+        $sql[$name][]= '("'.$word.'", ' . $count . ')';
+}
+foreach ($sql as $table => $values){
+        $query = "INSERT INTO $table (word, count) VALUES ".implode(',', $values) ." ON DUPLICATE KEY UPDATE count = count + VALUES(count), created = NOW()";
         $result = $conn->query($query);
 }
