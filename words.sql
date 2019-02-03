6a7ab711181367fd056e7b4ba8cd0267af0aeb9f
Bob Carlson <b0bcarlson@users.noreply.github.com>
2019-02-03 05:47:10 -0600

    Update words.sql

--- words.sql
+++ words.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 13, 2019 at 07:34 AM
--- Server version: 5.7.24-0ubuntu0.18.04.1
+-- Generation Time: Feb 03, 2019 at 05:46 AM
+-- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.2.10-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -22,13 +22,16 @@ USE `words`;
 -- Table structure for table `words1`
 --
 
-CREATE TABLE `words1` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `words1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(256) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '1',
   `start` int(11) NOT NULL DEFAULT '0',
   `end` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`),
+  KEY `count` (`count`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -37,13 +40,16 @@ CREATE TABLE `words1` (
 -- Table structure for table `words2`
 --
 
-CREATE TABLE `words2` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `words2` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(256) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '1',
   `start` int(11) NOT NULL DEFAULT '0',
   `end` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`),
+  KEY `count` (`count`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -52,13 +58,16 @@ CREATE TABLE `words2` (
 -- Table structure for table `words3`
 --
 
-CREATE TABLE `words3` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `words3` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(256) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '1',
   `start` int(11) NOT NULL DEFAULT '0',
   `end` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`),
+  KEY `count` (`count`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -67,13 +76,16 @@ CREATE TABLE `words3` (
 -- Table structure for table `words4`
 --
 
-CREATE TABLE `words4` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `words4` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(256) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '1',
   `start` int(11) NOT NULL DEFAULT '0',
   `end` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`),
+  KEY `count` (`count`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -82,80 +94,14 @@ CREATE TABLE `words4` (
 -- Table structure for table `words5`
 --
 
-CREATE TABLE `words5` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `words5` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `word` varchar(256) NOT NULL,
   `count` int(11) NOT NULL DEFAULT '1',
   `start` int(11) NOT NULL DEFAULT '0',
   `end` int(11) NOT NULL DEFAULT '0',
-  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `word` (`word`),
+  KEY `count` (`count`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `words1`
---
-ALTER TABLE `words1`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `word` (`word`);
-
---
--- Indexes for table `words2`
---
-ALTER TABLE `words2`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `word` (`word`);
-
---
--- Indexes for table `words3`
---
-ALTER TABLE `words3`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `word` (`word`);
-
---
--- Indexes for table `words4`
---
-ALTER TABLE `words4`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `word` (`word`);
-
---
--- Indexes for table `words5`
---
-ALTER TABLE `words5`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `word` (`word`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `words1`
---
-ALTER TABLE `words1`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `words2`
---
-ALTER TABLE `words2`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `words3`
---
-ALTER TABLE `words3`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `words4`
---
-ALTER TABLE `words4`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `words5`
---
-ALTER TABLE `words5`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
