072443d665827bf540b17082529a954da7cadb1f
Bob Carlson <b0bcarlson@users.noreply.github.com>
2019-02-10 19:03:03 -0600

    Update gen.php
    It works! But now as I am using it, I am finding that getwords.py is still not doing a very good job of filtering bots.

--- gen.php
+++ gen.php
@@ -2,25 +2,30 @@
 $plen=4;
 include("config.php");
 
-  function getRandomWeightedElement(array $weightedValues) {
-    $rand = mt_rand(1, (int) array_sum($weightedValues));
-
-    foreach ($weightedValues as $key => $value) {
-      $rand -= $value;
-      if ($rand <= 0) {
-        return $key;
-      }
-    }
-  }
+function getRandomWeightedElement(array $weightedValues) {
+        $rand = mt_rand(1, (int) array_sum($weightedValues));
+        foreach ($weightedValues as $key => $value) {
+                $rand -= $value;
+                if ($rand <= 0)
+                        return $key;
+        }
+}
 function helper($sen, $conn, $plen, $name) {
         $rand = sha1(serialize($sen));
+        $searchstring=implode(" ",array_slice($sen,-1*($plen)));
+        $query="SELECT `count`,`end` FROM `$name` WHERE `word`='$searchstring' LIMIT 1";
+        $result=$conn->query($query);
+        if($result->num_rows==1){
+                $row=$result->fetch_assoc();
+                if(rand(1,$row['count'])<=$row['end'])
+                        return $sen;
+        }
         $searchstring=implode(" ",array_slice($sen,-1*($plen-1)))." %";
         $query="SELECT `id`,`count` FROM `$name` WHERE `word` LIKE '$searchstring' AND `count` > 5 ORDER BY `count` DESC LIMIT 10";
         $result=$conn->query($query);
         for ($set = array (); $row = $result->fetch_assoc(); $set[$row['id']] = $row['count']);
-        if($result->num_rows==0){
+        if($result->num_rows==0)
                 return False;
-        }
         while(True){
                 $id = getRandomWeightedElement($set);
                 $query = "SELECT `word`,`count`,`end` FROM `$name` WHERE `id`=$id LIMIT 1";
@@ -30,15 +35,10 @@ function helper($sen, $conn, $plen, $name) {
                 $tsen=$sen;
                 $tsen[]=end($word);
                 $helperresult=helper($tsen,$conn,$plen,$name);
-                if($helperresult===False){
+                if($helperresult===False)
                         unset($set[$id]);
-                        echo"\nNUMBER OF ROWS REMAINING: ".count($set)." AT POINT $rand\n";
-                }
-                else{
+                else
                         return $helperresult;
-                }
-                echo implode(" ",$sen)."\n";
-                echo $row['end']."/".$row['count']."\n";
                 if(count($set)==0)
                         return False;
         }
